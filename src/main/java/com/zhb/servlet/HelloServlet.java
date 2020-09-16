6ede76ab08cbfa05f2d8b2afb633c22df063ceb3
赵宏博 <17763169636@163.com>
2020-09-16 20:30:22 +0800

    first

---
+++ src/main/java/com/zhb/servlet/HelloServlet.java
@@ -0,0 +1,20 @@
+package com.zhb.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloServlet",urlPatterns = {"/hello"})
+public class HelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=utf-8");
+        response.getWriter().println("hello world");
+    }
+}
